00fd8aae15614017d908284bc906c4a9bf903615
R.Vollet ❤️ <ronald.vollet@drogal.com.br>
2026-05-17 01:13:01 -0300

    feat: melhorias no nginx

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,7 @@ FROM nginx:stable-alpine
 RUN rm -rf /usr/share/nginx/html/*
 
 COPY --from=build /app/dist /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
 
--- src/routing.tsx
+++ src/routing.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router"
 import { ScheduleRoutes } from "./config"
 import ChoiceDataAndTimePage from "./page/ChoiceDataAndTimePage"
 import ChoiceServicePage from "./page/ChoiceServicePage"
-import DesignSystemPage from "./page/DesignSystemPage"
 import ProfilePage from "./page/ProfilePage"
 import ValidationPage from "./page/ValidationPage"
 import Header from "./shared/Header"
@@ -13,7 +12,7 @@ const Routing = () => {
     <Header />
     <main>
       <Routes>
-        <Route index element={<DesignSystemPage />} />
+        <Route index element={<ChoiceServicePage />} />
         <Route path={ScheduleRoutes.SERVICE} element={<ChoiceServicePage />} />
         <Route path={ScheduleRoutes.DATE_TIME} element={<ChoiceDataAndTimePage />} />
         <Route path={ScheduleRoutes.PROFILE} element={<ProfilePage />} />
